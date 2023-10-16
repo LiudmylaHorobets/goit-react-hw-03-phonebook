c4b10999089f2e8616fc6ad15e1d2f1e7dba608e
LiudmylaHorobets <horobets1323@gmail.com>
2023-10-16 13:45:57 +0300

    added localstorage on App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,6 @@ export class App extends Component {
   state = {
     contacts: [
       // DATA:
-      
       // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
@@ -54,6 +53,22 @@ export class App extends Component {
       };
     });
   };
+
+  // localstorage
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   render() {
     return (
